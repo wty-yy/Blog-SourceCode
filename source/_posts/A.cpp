3b49b3c5b5673d8698b2c36a18de63a50b98c3d1
wty-yy <993660140@qq.com>
2021-08-21 17:58:53 +0800

    x

---
+++ source/_posts/A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define db double
+#define ll long long
+#define int ll
+#define vi vector<int>
+#define vii vector<vi >
+#define pii pair<int, int>
+#define vp vector<pii >
+#define vip vector<vp >
+#define mkp make_pair
+#define pb push_back
+#define Case(x) cout << "Case #" << x << ": "
+using namespace std;
+const int INF = 0x3f3f3f3f;
+const int P = 998244353;
+const int N = 5e4 + 10;
+signed main(){
+#ifdef _DEBUG
+//	FILE *file = freopen("out", "w", stdout);
+#endif
+	int a;
+	if (a == 1) {
+		cout << 1 << '\n';
+	}
+	return 0;
+}
+
